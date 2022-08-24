a7789db9e3b172b83e888b5bd2b57b8785bf6fe9
Sinan Yumurtacı <sinan.yumurtaci@gmail.com>
2022-08-24 18:04:01 -0500

    simplify resume

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,26 +1,24 @@
 import './App.css';
 
-function Break() {
-	return (
-		<span>
-			<br/><br/>
-		</span>
-	)
-}
-
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <p>
-	  		<span id="header-top">
-			<span className="rainbow-text">Hey, I'm Sinan</span> </span><br/> I worked as a <a>Software Engineer Intern at Google</a>, and I'll gradute <a>Colby College</a> in May 2023.
-			<Break/>
-	  		I'm a passionate and curious software engineer. I enjoy designing and implementing systems that churn data.
-			<Break/>
-	  		Currently, I'm building strate.gg where you can play strategy games and looking for a company to join after I graduate.
-			<Break/>
-	  		See a copy of my resume. If interested, shoot me an email?
+			<p id="header-top">
+				<span className="rainbow-text">Hey, I'm Sinan</span>
+			</p>
+
+			<p id="header-second">
+	  		<ol>
+				<li>I was a <a>Software Engineer Intern at Google</a></li>
+	  			<li>I'll gradute <a>Colby College</a> in May 2023</li>
+				<li>I'm coding <a>strate.gg</a> as my hobby project</li>
+	  		</ol>
+
+		I'm looking to join a company using data in exciting ways.<br/>
+		See a copy of my resume, and shoot me an email?
+			</p>
         </p>
       </header>
     </div>
