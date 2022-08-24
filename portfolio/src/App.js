359068daa8bf89edf86b51aa532890b1e438c1f5
Sinan Yumurtacı <sinan.yumurtaci@gmail.com>
2022-08-24 17:29:38 -0500

    most minimal starting portfolio

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,11 +1,24 @@
 import './App.css';
 
+function Break() {
+	return (
+		<span>
+			<br/><br/>
+		</span>
+	)
+}
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <p>
-			Hey, I'm Sinan. I've interned as a Software Engineer at Google, and I'll gradute in May 2023.
+			Hey, I'm Sinan. I was a Software Engineer Intern at Google, and I'll gradute Colby College in May 2023.
+			<Break/>
+	  		I'm a passionate and curious software engineer. I enjoy designing and implementing systems that churn data.
+			<Break/>
+	  		Currently, I'm building strate.gg where you can play strategy games and looking for a company to join after I graduate.
+			<Break/>
+	  		See a copy of my resume. If interested, shoot me an email?
         </p>
       </header>
     </div>
