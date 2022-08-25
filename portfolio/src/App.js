44c3fe50afafab4eb177428fe3bfeb882f1e4d11
Sinan Yumurtacı <sinan.yumurtaci@gmail.com>
2022-08-24 21:09:36 -0500

    rainbox box to the right!

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -4,18 +4,20 @@ function App() {
   return (
 	<div className="App">
 		<header className="App-header">
-		<p id="header-top">
-			<span className="rainbow-text">Hey, I'm Sinan</span><span className="hidden-text">, and this is a bit about me</span>
-		</p>
+		<div>
+			<p id="header-top">
+				<span className="rainbow-text">Hey, I'm Sinan</span><span className="hidden-text">, and this is a bit about me</span>
+			</p>
 
-			<p className="header-second">
-			<ol className="no-margin">
-				<li>I was a <span href="#google-intern">Software Engineering Intern at Google</span></li>
-				<li>I'll graduate <span href="#education-colby">Colby College</span> in May 2023</li>
-				<li>I'm coding <span href="#projects-strategg">strate.gg</span> as a hobby project</li>
-			</ol>
-		</p>
-		<div className="horizontal-center">
+				<p className="header-second">
+				<ol className="no-margin">
+					<li>I was a <span href="#google-intern">Software Engineering Intern at Google</span></li>
+					<li>I'll graduate <span href="#education-colby">Colby College</span> in May 2023</li>
+					<li>I'm coding <span href="#projects-strategg">strate.gg</span> as a hobby project</li>
+				</ol>
+			</p>
+		</div>
+		<div className="horizontal-right">
 			<div className="rainbow-box">
 				<p className="header-second">
 					I'm looking to join a company using data in exciting ways.<br/>
