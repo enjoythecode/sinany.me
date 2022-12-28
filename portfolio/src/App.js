eb2a1da57c10b0970684920c0ff106d0115848f8
Sinan Yumurtaci <sinan.yumurtaci@gmail.com>
2022-12-28 15:12:08 -0600

    updated call-to-action

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -10,19 +10,19 @@ function App() {
 			</p>
 
 			<p className="highlight-paragraph narrow">
-				I <span class="rainbow-text">interned at Google</span> last summer, using <span className="skill">Java</span> and <span className="skill">Go</span> to help build a knowledge graph with trillions of data points. 
+				I <span className="rainbow-text">interned at Google</span> last summer, using <span className="skill">Java</span> and <span className="skill">Go</span> to help build a knowledge graph with trillions of data points. 
 			</p>
 
 			<p className="highlight-paragraph narrow">
-				I'm developing a <span class="rainbow-text">real-time strategy game website</span> using <span className="skill">Python (Flask)</span> and <span className="skill">Javascript (React)</span>.
+				I'm developing a <span className="rainbow-text">real-time strategy game website</span> using <span className="skill">Python (Flask)</span> and <span className="skill">Javascript (React)</span>.
 			</p>
 
 		</div>
 		<div className="horizontal-right">
 			<div className="rainbow-box">
-				<p className="header-second">
-					I'm looking to join a company using data in exciting ways.<br/>
-					Check out my <a href="/Sinan_Yumurtaci_Resume.pdf">resume</a>, and <a href="mailto:sinan.yumurtaci@gmail.com">let's chat!</a>
+				<p className="highlight-paragraph narrow" style={{"marginTop": 0}}>
+					I am looking to join a team using data in exciting ways.<br style={{"marginBottom": "0.6em"}}/>
+					Check out my <a href="/Sinan_Yumurtaci_Resume.pdf">resume</a> and <a href="mailto:sinan.yumurtaci@gmail.com">let's chat!</a>
 				</p>
 			</div>
 		</div>
