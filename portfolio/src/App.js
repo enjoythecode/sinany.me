19707ed0a25f873fe1cf2c651bdca314c9da51a2
Sinan Yumurtaci <sinan.yumurtaci@gmail.com>
2023-01-27 19:25:35 -0600

    portfolio presentation improvements, add 98point6

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,43 +1,99 @@
 import './App.css';
 
 function App() {
+
+	const EXPERIENCE_TYPE = {
+		Employment: "Employment",
+		Project: "Project",
+	}
+	const SKILL = {
+		Python: "Python",
+		Flask: "Flask",
+		Pytest: "Pytest",
+
+		Redis: "Redis",
+		Docker: "Docker",
+
+		Java: "Java",
+		Go: "Go",
+		GoogleCloud: "Google Cloud",
+		PerformanceOptimization: "Performance Optimization",
+
+		Javascript: "Javascript",
+		React: "React",
+		Jest: "Jest",
+
+		Git: "Git",
+
+		Bash: "Bash",
+	}
+	const resume = {
+		"experience": {
+			"google":
+			{
+				"type": EXPERIENCE_TYPE.Employment,
+				"skills": [SKILL.Java, SKILL.Go, SKILL.Bash, SKILL.GoogleCloud, SKILL.PerformanceOptimization, SKILL.Git,]
+			}
+		}
+	};
+
+
   return (
 	<div className="App">
-		<header className="App-header">
-		<div>
-			<h1>
-				<span className="rainbow-text">Hey, I'm Sinan! </span> 
-					I am a software engineering student looking to join a team of smart, passionate problem solvers.
-				
-			</h1>
-
-			<p><span className="hiddenarst-text">I am  graduating this May 2023; {/*<br style={{"marginBottom": "0.6em"}}/>*/}
-				check out my <a href="/Sinan_Yumurtaci_Resume.pdf">resume</a>, and <a href="mailto:sinan.yumurtaci@gmail.com">let's chat!</a></span></p>
-
-			<hr></hr>
-
-			<h2>Professional Experience</h2>
-
-			<p className="highlight-paragraph narrow">
-				I <span className="rainbow-text">interned at Google</span> last summer, using 
-				<span className="skill"> Java</span> and <span className="skill">Go</span> to
-				help build a knowledge graph with trillions of data points. 
-			</p>
-
-			<p className="highlight-paragraph narrow">
-				I'm developing a <span className="rainbow-text">real-time strategy game website</span> using
-				<span className="skill"> Python (Flask)</span> and <span className="skill">Javascript (React)</span>.
-			</p>
-
-		</div>
-		<div className="horizontal-right">
-			<div className="rainbow-box">
-				<p className="highlight-paragraph narrow" style={{"marginTop": 0}}>
+		<header className="App-landing">
+			<div>
+				<h1>
+					<span className="rainbow-text">Hi, I'm Sinan! </span> 
+						I am a software engineering student looking to join a team of smart, passionate problem solvers.
 					
-				</p>
+				</h1>
+
+				<p><span>I am  graduating May 2023; {/*<br style={{"marginBottom": "0.6em"}}/>*/}
+					check out my <a className="rainbow-link" href="/Sinan_Yumurtaci_Resume.pdf">resume</a> (or read below), and <a className="rainbow-link" href="mailto:sinan.yumurtaci@gmail.com">let's chat!</a></span></p>
+			</div>
+		</header>
+
+
+		<div className="resume-item narrow">
+			<p>
+				I interned at <span className="rainbow-text">Google</span> last summer, helping Data Commons develop a knowledge graph of over a billion time series.
+				<br/>
+				<br/>
+				<span className="hidden-text">I improved the efficiency of their development processes by 30%, among multiple other contributions.</span>
+			</p>
+			
+			<div className="hidden">
+			<br/>
+				<a>
+					<span className="resume-item-cta"><i>click to learn more »</i></span>
+				</a>
 			</div>
 		</div>
-      </header>
+
+		<div className="resume-item narrow">
+			<p>
+				I'm developing <span className="rainbow-text">strate.gg</span>, a real-time strategy website for games such as Amazons,
+				Mancala, and Chess.
+
+				<br/>
+				<br/>
+				<span className="hidden-text">This is a full-stack development project where I develop my skills in automated testing with CI, 
+				containerized deployments with Docker, React, and Redis.</span>
+			</p>
+		</div>
+
+		<div className="resume-item narrow">
+			<p>
+				I interned at <span className="rainbow-text">98point6</span> during Summer 2021, contributing to their mission to provide affordable healthcare.
+			
+				<br/>
+				<br/>
+				<span className="hidden-text">I designed and implemented a recommender system poised to save 120 physicians-hours (equivalent to $10,000) per year using Machine Learning.</span>
+			</p>
+		</div>
+		
+
+      
     </div>
   );
 }
