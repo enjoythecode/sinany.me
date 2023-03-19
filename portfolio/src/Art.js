1cd2d37e32953f1a9b3e7cfb6276b150d6d6c12a
Sinan Yumurtacı <sinan.yumurtaci@gmail.com>
2023-03-18 21:27:52 -0400

    add art gift; crossing, 2023-03-18

--- portfolio/src/Art.js
+++ portfolio/src/Art.js
@@ -1,18 +1,26 @@
 import shuffling_squares from "./assets/art/shuffling_squares.gif"
 import its_raining from "./assets/art/its_raining.gif"
+import crossing from "./assets/art/crossing.gif"
 
 function Art() {
   return (
-	<div style={{"background": "black", "height": "100vh", "textAlign": "center"}}>
-        <img src={shuffling_squares} alt="GIF Art; Shuffling Squares, by Sinan Yumurtaci"/>
+	<div style={{"background": "black", "height": "100%", "textAlign": "center"}}>
+        <img src={crossing} style={{"width":"400px"}} alt="GIF Art; Crossing, by Sinan Yumurtaci"/>
         <br/>
-        <i>Shuffling Squares, 2023-02-27</i>
+        <i>Crossing</i>, 2023-03-18
 
         <br/>
 
         <img src={its_raining} style={{"width": "400px"}} alt="GIF Art; Its  by Sinan Yumurtaci"/>
         <br/>
-        <i>It's Raining, 2023-02-28</i>
+        <i>It's Raining</i>, 2023-02-28
+
+        <br/>
+
+        <img src={shuffling_squares} alt="GIF Art; Shuffling Squares, by Sinan Yumurtaci"/>
+        <br/>
+        <i>Shuffling Squares</i>, 2023-02-27
+
     </div>
   );
 }
