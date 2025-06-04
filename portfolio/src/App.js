8ab83400063381739fd6197262a79ff382d85ca7
Sinan Yumurtaci <sinan.yumurtaci@gmail.com>
2025-06-03 22:17:35 -0500

    a build of fizik that i am proud of :)

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -38,30 +38,30 @@ function App() {
 	};*/
 
 
-  return (
-	<div className="App">
-		<header className="App-landing">
-			<div>
-				<h1>
-					<span className="rainbow-text">Hi, I'm Sinan! </span> 
+	return (
+		<div className="App">
+			<header className="App-landing">
+				<div>
+					<h1>
+						<span className="rainbow-text">Hi, I'm Sinan! </span>
 						I am a software engineer at <a className="rainbow-link" href='https://bloomberg.com/' target='_blank' rel="noreferrer">Bloomberg LP</a> working with a team of smart, passionate problem solvers.
-					
-				</h1>
 
-				<p><span>Check out my <a className="rainbow-link" href="/Sinan_Yumurtaci_Resume.pdf">resume</a> (or read below), and <a className="rainbow-link" href="mailto:sinan.yumurtaci@gmail.com">let's chat!</a></span></p>
-			</div>
-		</header>
+					</h1>
+
+					<p><span>Check out my <a className="rainbow-link" href="/Sinan_Yumurtaci_Resume.pdf">resume</a> (or read below), and <a className="rainbow-link" href="mailto:sinan.yumurtaci@gmail.com">let's chat!</a></span></p>
+				</div>
+			</header>
 
 
-		<div className="resume-item narrow">
-			<p>
-				I interned at <span className="rainbow-text">Google</span> last summer, helping Data Commons develop a knowledge graph of over a billion time series.
-				<br/>
-				<br/>
-				<span className="hidden-text">I improved the efficiency of their development tooling by 30%, among multiple other contributions.</span>
-			</p>
-			
-			{/*
+			<div className="resume-item narrow">
+				<p>
+					I interned at <span className="rainbow-text">Google</span> in 2022, helping Data Commons develop a knowledge graph of over a billion time series.
+					<br />
+					<br />
+					<span className="hidden-text">I improved the efficiency of their development tooling by 30%, among multiple other contributions.</span>
+				</p>
+
+				{/*
 			<div>
 			<br/>
 				<a>
@@ -69,34 +69,34 @@ function App() {
 				</a>
 			</div>
 			*/}
+			</div>
+
+			<div className="resume-item narrow">
+				<p>
+					I'm prototyped <span className="rainbow-text">strate.gg</span>, a real-time strategy website for games such as Amazons,
+					Mancala, and Chess.
+
+					<br />
+					<br />
+					<span className="hidden-text">This is a full-stack development project where I developed my skills in automated testing with CI,
+						containerized deployments with Docker, React, and Redis.</span>
+				</p>
+			</div>
+
+			<div className="resume-item narrow">
+				<p>
+					I interned at <span className="rainbow-text">98point6</span> in 2021, contributing to their mission to provide affordable healthcare.
+
+					<br />
+					<br />
+					<span className="hidden-text">I designed and implemented a recommender system poised to save 120 physicians-hours per year using Machine Learning.</span>
+				</p>
+			</div>
+
+
+
 		</div>
-
-		<div className="resume-item narrow">
-			<p>
-				I'm developing <span className="rainbow-text">strate.gg</span>, a real-time strategy website for games such as Amazons,
-				Mancala, and Chess.
-
-				<br/>
-				<br/>
-				<span className="hidden-text">This is a full-stack development project where I develop my skills in automated testing with CI, 
-				containerized deployments with Docker, React, and Redis.</span>
-			</p>
-		</div>
-
-		<div className="resume-item narrow">
-			<p>
-				I interned at <span className="rainbow-text">98point6</span> during Summer 2021, contributing to their mission to provide affordable healthcare.
-			
-				<br/>
-				<br/>
-				<span className="hidden-text">I designed and implemented a recommender system poised to save 120 physicians-hours (equivalent to $10,000) per year using Machine Learning.</span>
-			</p>
-		</div>
-		
-
-      
-    </div>
-  );
+	);
 }
 
 export default App;
